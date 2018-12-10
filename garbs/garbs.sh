376d20e5435062bde588eb0880eb9d3bc624da06
Gauge Krahe <gauge.krahe@gmail.com>
2018-12-10 20:28:51 +1000

    Updated garbs to hopefully only install nvidia drivers if a nvidia card is detected

--- garbs/garbs.sh
+++ garbs/garbs.sh
@@ -299,6 +299,12 @@ systemctl enable sddm
 # Most important command! Get rid of the beep!
 systembeepoff
 
+if [[ -n $(lspci | grep NVIDIA) ]]; then
+	pacman -S --noconfirm nvidia nvidia-utils lib32-nvidia-utils nvidia-settings 
+fi
+
+#nvidia nvidia-utils lib32-nvidia-utils nvidia-settings
+
 # This line, overwriting the `newperms` command above will allow the user to run
 # serveral important commands, `shutdown`, `reboot`, updating, etc. without a password.
 newperms "%wheel ALL=(ALL) ALL\\n%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay"
