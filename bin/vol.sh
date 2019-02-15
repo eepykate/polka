360c3a37a19f7be5d4cbcbee649ab1e530b233fc
Gauge Krahe <gauge.krahe@gmail.com>
2019-02-16 09:54:57 +1000

    Whoops, forgot the volume script

---
+++ bin/vol.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pactl="$(pulseaudio-ctl full-status)"
+vol="$(echo ${pactl}  | awk '{print $1 "%"}')"
+mute="$(echo ${pactl} | awk '{print $2}')"
+
+if [ $mute = yes ]; then
+    output="🔇 Muted"
+elif [ $mute = no ]; then
+    output="🔊 ${vol}"
+fi
+
+##           🔊    🔇    
+
+echo "$output"
