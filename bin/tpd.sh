27185e4935a328a9d0373b6e6052de8187eb83f6
Gauge Krahe <gauge.krahe@gmail.com>
2019-02-17 03:12:58 +1000

    Fuck this script

--- bin/tpd.sh
+++ bin/tpd.sh
@@ -1,34 +1,20 @@
 #!/bin/bash
 
-#sleep 5;
+sleep 3
 
-dar() {
-    #Detect the name of the display in use
-    display=":$(/usr/bin/ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
-
-    #Detect the user using such display
-    user=$(who | grep '('$display')' | awk '{print $1}' | sed -n -e 1p)
-
-    #Detect the id of the user
-    uid=$(id -u $user)
-
-    sudo -u $user DISPLAY=$display DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus "$@"
-}
-
-mice=(/sys/class/input/mouse*)
-mice=("${mice[@]//*mouse}")
-
-dev=" "
-
-
-for mouse in "${mice[@]}"; do
-    export dev="$(udevadm info /sys/class/input/mouse${mouse} | awk '$1 ~ "E:"' | awk '/ID_TYPE/' | awk '{$1=""; print $2}')$dev"
+printf "" > /tmp/tpd
+ 
+for mouse in /sys/class/input/mouse*; do
+    type="$(udevadm info ${mouse} | awk '$1 ~ "E:"' | awk '/ID_TYPE/' | awk '{$1=""; print $2}')"
+    echo "${type}" >> /tmp/tpd
 done
 
-if [[ -n $(echo "${dev}" | grep "hid") ]]; then
-    dar /usr/bin/synclient TouchpadOff=1
+type="$(cat /tmp/tpd)"
+
+sleep 1
+
+if [[ $(echo "$type" | grep -i hid | wc -m) -ge 5 ]]; then
+    /usr/bin/synclient TouchpadOff=1
 else
-    dar /usr/bin/synclient TouchpadOff=0
+    /usr/bin/synclient TouchpadOff=0
 fi
-
-
