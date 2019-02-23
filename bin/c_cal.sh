07c440e050dab157b5dbede1a637b467eeae94c2
Gauge Krahe <gauge.krahe@gmail.com>
2019-02-23 10:40:29 +1000

    Updated c_cal.sh - now `printf`s the output

--- bin/c_cal.sh
+++ bin/c_cal.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
 day="$(date +%d)"
 cal="$(cal | sed -e "s/$day/\<span color=\'#4d9de0\'\>\<b\>$day\<\/b\>\<\/span\>/" -e '1d' -e '8d' -e 's/$/​/')"
 top="$(cal | sed '1!d')"
+cal1="$(cal | sed -e "s/$day/\\\\e\\[1;34m$day\\\\e\\[0m/" -e '8d' -e 's/$/​/')"
+
+printf "$cal1\n" 
 
 notify-send "$top" "$cal"
