733944fc775f138081c057c5c8222b807bcdea69
Gauge Krahe <gauge.krahe@gmail.com>
2019-03-03 02:27:57 +1000

    Updated c_cal.sh

--- bin/c_cal.sh
+++ bin/c_cal.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-day="$(date +%d)"
-cal="$(cal | sed -e "s/$day/\<span color=\'#4d9de0\'\>\<b\>$day\<\/b\>\<\/span\>/" -e '1d' -e '8d' -e 's/$/​/')"
-top="$(cal | sed '1!d')"
-cal1="$(cal | sed -e "s/$day/\\\\e\\[1;34m$day\\\\e\\[0m/" -e '8d' -e 's/$/​/')"
+calsh="$(cal | sed 's/$/ /g' )"
+day="$(date +%d | sed -e 's/^0/ /' -e 's/ .$/\0/')"
+cal="$(echo "$calsh" | sed  -e "s/$day/\<span color=\'#02a4fc\'\>\<b\>$day\<\/b\>\<\/span\>/" -e '1d' -e '8d' -e 's/ $//g' -e 's/ <\/b><\/span>$/<\/b><\/span>/' -e 's/$/​/')"
+top="$(echo "$calsh" | sed '1!d')"
+cal1="$(echo "$calsh" | sed -e "s/$day/\\\\e\\[1;34m$day\\\\e\\[0m/" -e '8d' -e 's/ $//' -e 's/ $//' -e 's/ \\e\[0m$/\\e\[0m/' -e 's/$/​/')"
 
 printf "$cal1\n" 
 
