858bdfe3aa50e15a664b92ad1056e3745feb4d52
Gauge Krahe <gaugekrahe@gmail.com>
2019-03-08 22:49:30 +1000

    Updated c_bat.sh, p_bat.sh, and pure.tint2rc

--- bin/c_bat.sh
+++ bin/c_bat.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 # Give a battery name (e.g. BAT0) as an argument.
 
-capacity=$(cat /sys/class/power_supply/"$1"/capacity) || exit
-status=$(cat /sys/class/power_supply/"$1"/status)
+#capacity=$(cat /sys/class/power_supply/"$1"/capacity) || exit
+#status=$(cat /sys/class/power_supply/"$1"/status)
+#notify-send "Battery percentage is at $(echo "$capacity" | sed -e 's/$/%/')"
 
-notify-send "Battery percentage is at $(echo "$capacity" | sed -e 's/$/%/')"
+acpi="$(acpi)"
+
+notify-send "$(acpi)"
--- bin/p_bat.sh
+++ bin/p_bat.sh
@@ -9,6 +9,19 @@ done
 capacity=$(cat /sys/class/power_supply/"$1"/capacity) || exit
 status=$(cat /sys/class/power_supply/"$1"/status)
 
+case "$capacity" in
+    [0-9]|1[0-9])         bat="" ;;
+    2[0-9])               bat="" ;;
+    3[0-9])               bat="" ;;
+    4[0-9])               bat="" ;;
+    5[0-9])               bat="" ;;
+    6[0-9])               bat="" ;;
+    7[0-9])               bat="" ;;
+    8[0-9])               bat="" ;;
+    9[0-9])               bat="" ;;
+    *)                    bat="" ;;
+esac
+
 if [ "$capacity" -ge 75 ]; then
 	color="$color10"
 elif [ "$capacity" -ge 50 ]; then
@@ -24,4 +37,4 @@ fi
 
 [ "$status" = "Charging" ] && color="$color15"
 
-printf "⚡︎ <span color='%s'>%s%s</span>"  "$color"  "$warn"  "$(echo "$capacity" | sed -e 's/$/%/')"
+printf "${warn}${bat}"
