2849b0dd7f781e2073bb38bf9a04800104536660
Gauge Krahe <gauge.krahe@gmail.com>
2019-04-01 19:03:32 +1000

    Hopefully fixed c_cal.sh

--- bin/c_cal.sh
+++ bin/c_cal.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-calsh="$(cal | sed 's/$/ /g' )"
-day="$(date +%d | sed -e 's/^0/ /' -e 's/ .$/\0/')"
-cal="$(echo "$calsh" | sed  -e "s/$day/\<span color=\'#02a4fc\'\>\<b\>$day\<\/b\>\<\/span\>/" -e '1d' -e '8d' -e 's/ $//g' -e 's/ <\/b><\/span>$/<\/b><\/span>/' -e 's/$/​/')"
+day="$(date +%d | sed -e 's/^0/ /')"
+calsh="$(cal | sed -e 's/$/ /g' -e 's/^/ /' )"
+cal="$(echo "$calsh" | sed  -e "s/ $day / \<span color=\'#02a4fc\'\>\<b\>$day\<\/b\>\<\/span\> /" -e '1d' -e '8d' -e 's/ $//g' -e 's/ <\/b><\/span>$/<\/b><\/span>/' -e 's/$/​/' -e 's/^ //')"
 top="$(echo "$calsh" | sed '1!d')"
-cal1="$(echo "$calsh" | sed -e "s/$day/\\\\e\\[1;34m$day\\\\e\\[0m/" -e '8d' -e 's/ $//' -e 's/ $//' -e 's/ \\e\[0m$/\\e\[0m/' -e 's/$/​/')"
+cal1="$(echo "$calsh" | sed -e "s/ $day / \\\\e\\[1;34m$day\\\\e\\[0m /" -e '8d' -e 's/ $//' -e 's/ $//' -e 's/ \\e\[0m$/\\e\[0m/' -e 's/$/​/' -e 's/^ //')"
 
 printf "$cal1\n" 
 
