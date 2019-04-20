e864412502216c6ce377295c0fc123bf499844ca
Gauge Krahe <gaugekrahe@gmail.com>
2019-04-20 15:28:58 +0000

    Remove useless lines from tp.sh

--- bin/tp.sh
+++ bin/tp.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-opts="$(getopt -o h,u --long hide,unhide -- "$@")"
-eval set -- "$opts"
-
 if [[ $(command cat /tmp/panelstatus) = windowunmap ]]; then
 	thing="windowmap" &&
 	echo "windowmap" > /tmp/panelstatus
