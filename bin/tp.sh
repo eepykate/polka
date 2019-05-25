d70ff5ca89e14c78f0ec47a1bd221ebcea28e7da
Gauge Krahe <gaugekrahe@gmail.com>
2019-05-25 18:06:57 +1000

    Made tp.sh use bspwm and polybar - updated polybar conf

--- bin/tp.sh
+++ bin/tp.sh
@@ -3,13 +3,14 @@
 if [[ $(command cat /tmp/panelstatus) = windowunmap ]]; then
 	thing="windowmap" &&
 	echo "windowmap" > /tmp/panelstatus
+	bspc config -m HDMI-0 top_padding 38
 else
 	thing="windowunmap" &&
 	echo "windowunmap" > /tmp/panelstatus
+	bspc config -m HDMI-0 top_padding 0
 fi
 
-ids=$(xdotool search --name "tint2")
+ids=$(xdotool search --name "polybar")
 for aaa in ${ids[@]}; do
 	xdotool $(printf $thing) $aaa
 done
-
