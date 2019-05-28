7a15d3c3a81180cd116fdd50444fe081998fad10
Gauge Krahe <gaugekrahe@gmail.com>
2019-05-28 19:05:09 +1000

    Updated polybar.sh

--- bin/polybar.sh
+++ bin/polybar.sh
@@ -9,6 +9,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config main &
+MONITOR="$(xrandr -q | grep primary | awk '{print $1}')" \
+		polybar -c ~/.config/polybar/config main &!
 
 nm-applet &!
