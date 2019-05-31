e66697e43e4f05285e1b16edafcd84dd5c01a70e
Gauge Krahe <gaugekrahe@gmail.com>
2019-05-31 19:17:26 +1000

    Hopefully only clicking on the active tab focuses the url bar

--- .mozilla/firefox/gauge.gauge/chrome/userChrome.js
+++ .mozilla/firefox/gauge.gauge/chrome/userChrome.js
@@ -8,15 +8,38 @@ XPCOMUtils.defineLazyGetter(FullScreen, "useLionFullScreen", function() {
 	return false;
 });
 
-var classname = document.getElementsByClassName("tabbrowser-tab");
-var addressBar= document.getElementById('urlbar');
+/*
+function tabClick() {
+	var classname = document.getElementsByClassName("tabbrowser-tab");
+	var addressBar= document.getElementById('urlbar');
 
-var myFunction = function() {
-  document.getElementById('urlbar').focus();
+	var myFunction = function() {
+		document.getElementById('urlbar').focus();
+	};
+	
+	for (var i = 0; i < classname.length; i++) {
+		classname[i].addEventListener('click', myFunction);
+	}
 };
 
-for (var i = 0; i < classname.length; i++) {
-    classname[i].addEventListener('click', myFunction);
-}
+setInterval(tabClick, 2000);
+*/
 
+var myFunction = function() {
+	document.getElementById('urlbar').focus();
+};
+
+function tabClick() {
+
+	var classname = document.getElementsByClassName("tabbrowser-tab");
+
+	for (var i = 0; i < classname.length; i++) {
+		classname[i].removeEventListener('click', myFunction);
+	}
+
+	document.querySelector(".tabbrowser-tab[selected=\"true\"]").addEventListener('click', myFunction);
+
+};
+
+setInterval(tabClick, 800);
 
