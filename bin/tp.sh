466d49901c4320c02dcc6368f0fbd3509ac4fee4
Gauge Krahe <gaugekrahe@gmail.com>
2019-06-04 14:49:11 +1000

    Updated bar1 and tp.sh - using it in xinitrc

--- bin/tp.sh
+++ bin/tp.sh
@@ -3,10 +3,11 @@
 if [[ $(command cat /tmp/panelstatus) = windowunmap ]]; then
 	thing="windowmap" &&
 	echo "windowmap" > /tmp/panelstatus
-	bspc config -m HDMI-0 top_padding 46
+	bspc config -m HDMI-0 top_padding 3
 else
 	thing="windowunmap" &&
 	echo "windowunmap" > /tmp/panelstatus
+	#bspc config -m HDMI-0 top_monocle_padding 0
 	bspc config -m HDMI-0 top_padding 0
 fi
 
