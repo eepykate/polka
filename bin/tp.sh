cf51cf8aa17774cf565baca3b55cb3364504accf
Gauge Krahe <gaugekrahe@gmail.com>
2019-06-06 02:28:52 +1000

    using normal bar again

--- bin/tp.sh
+++ bin/tp.sh
@@ -3,7 +3,8 @@
 if [[ $(command cat /tmp/panelstatus) = windowunmap ]]; then
 	thing="windowmap" &&
 	echo "windowmap" > /tmp/panelstatus
-	bspc config -m HDMI-0 top_padding 3
+	#bspc config -m HDMI-0 top_padding 3
+	bspc config -m HDMI-0 top_padding 44
 else
 	thing="windowunmap" &&
 	echo "windowunmap" > /tmp/panelstatus
