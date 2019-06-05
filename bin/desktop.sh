7f535339db70907809b0b6e1440ce39d0a190e94
Gauge Krahe <gaugekrahe@gmail.com>
2019-06-06 09:30:43 +1000

    Updated bar - made it more responsive

--- bin/desktop.sh
+++ bin/desktop.sh
@@ -4,42 +4,43 @@ desktop="$(xdotool get_desktop)"
 
 switch() {
 	if [[ $((${desktop} -1)) = -1 ]]; then
-	    desktops="$(($(xdotool get_num_desktops) -1))"
-	    case $1 in 
-	        next) xdotool set_desktop $((${desktop} +1));;
-	        previous) xdotool set_desktop ${desktops};;
-	    esac
+		desktops="$(($(xdotool get_num_desktops) -1))"
+		case $1 in 
+			next) xdotool set_desktop $((${desktop} +1));;
+			previous) xdotool set_desktop ${desktops};;
+		esac
 	elif [[ $((${desktop} +1)) = $(xdotool get_num_desktops) ]]; then
-	    case $1 in 
-	        next) xdotool set_desktop 0;;
-	        previous) xdotool set_desktop $((${desktop} -1));;
-	    esac
+		case $1 in 
+			next) xdotool set_desktop 0;;
+			previous) xdotool set_desktop $((${desktop} -1));;
+		esac
 	else
-	    case $1 in 
-	        next) xdotool set_desktop $((${desktop} +1));;
-	        previous) xdotool set_desktop $((${desktop} -1));;
-	    esac
+		case $1 in 
+			next) xdotool set_desktop $((${desktop} +1));;
+			previous) xdotool set_desktop $((${desktop} -1));;
+		esac
 	fi
+	bar -d
 }
 
 
 move() {
 	if [[ $((${desktop} -1)) = -1 ]]; then
-	    desktops="$(($(xdotool get_num_desktops) -1))"
-	    case $1 in 
-	        next) xdotool getwindowfocus set_desktop_for_window $((${desktop} +1));;
-	        previous) xdotool getwindowfocus set_desktop_for_window ${desktops};;
-	    esac
+		desktops="$(($(xdotool get_num_desktops) -1))"
+		case $1 in 
+			next) xdotool getwindowfocus set_desktop_for_window $((${desktop} +1));;
+			previous) xdotool getwindowfocus set_desktop_for_window ${desktops};;
+		esac
 	elif [[ $((${desktop} +1)) = $(xdotool get_num_desktops) ]]; then
-	    case $1 in 
-	        next) xdotool getwindowfocus set_desktop_for_window 1;;
-	        previous) xdotool getwindowfocus set_desktop_for_window $((${desktop} -1));;
-	    esac
+		case $1 in 
+			next) xdotool getwindowfocus set_desktop_for_window 1;;
+			previous) xdotool getwindowfocus set_desktop_for_window $((${desktop} -1));;
+		esac
 	else
-	    case $1 in 
-	        next) xdotool getwindowfocus set_desktop_for_window $((${desktop} +1));;
-	        previous) xdotool getwindowfocus set_desktop_for_window $((${desktop} -1));;
-	    esac
+		case $1 in 
+			next) xdotool getwindowfocus set_desktop_for_window $((${desktop} +1));;
+			previous) xdotool getwindowfocus set_desktop_for_window $((${desktop} -1));;
+		esac
 	fi
 }
 
