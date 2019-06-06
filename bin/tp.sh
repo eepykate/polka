7c7d6f59b75e520a273488e1dc0f7dd030db1bcc
Gauge Krahe <gaugekrahe@gmail.com>
2019-06-06 17:24:48 +1000

    Updated tp.sh

--- bin/tp.sh
+++ bin/tp.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env zsh
 
+monitor="$(xrandr | grep primary | awk '{print $1}')"
+
 if [[ $(command cat /tmp/panelstatus) = windowunmap ]]; then
 	thing="windowmap" &&
 	echo "windowmap" > /tmp/panelstatus
 	#bspc config -m HDMI-0 top_padding 3
-	bspc config -m HDMI-0 top_padding 44
+	bspc config -m $monitor top_padding 44
 else
 	thing="windowunmap" &&
 	echo "windowunmap" > /tmp/panelstatus
 	#bspc config -m HDMI-0 top_monocle_padding 0
-	bspc config -m HDMI-0 top_padding 0
+	bspc config -m $monitor top_padding 0
 fi
 
 IFS=$'\n' ids=( $(xdotool search --name "lemonbar") )
