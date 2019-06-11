69c3cc43eb6899733750b995e2124a238c466f8c
Gauge Krahe <gaugekrahe@gmail.com>
2019-06-11 16:54:09 +1000

    Removed margin from bar

--- bin/tp.sh
+++ bin/tp.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env zsh
 
-monitor="$(xrandr | grep primary | awk '{print $1}')"
-
 if [[ $(command cat /tmp/panelstatus) = windowunmap ]]; then
 	thing="windowmap" &&
 	echo "windowmap" > /tmp/panelstatus
 	#bspc config -m HDMI-0 top_padding 3
-	bspc config -m $monitor top_padding 44
+	bspc config -m HDMI-0 top_padding 30
 else
 	thing="windowunmap" &&
 	echo "windowunmap" > /tmp/panelstatus
 	#bspc config -m HDMI-0 top_monocle_padding 0
-	bspc config -m $monitor top_padding 0
+	bspc config -m HDMI-0 top_padding 0
 fi
 
 IFS=$'\n' ids=( $(xdotool search --name "lemonbar") )
