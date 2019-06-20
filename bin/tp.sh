674aa1f90e3a60f0aa009f3e6d1d55143441fc7a
Gauge Krahe <gaugekrahe@gmail.com>
2019-06-21 03:34:41 +1000

    Added mango font (From github/turquoise-hexagon too) - updated bar2 added bar3 for this font

--- bin/tp.sh
+++ bin/tp.sh
@@ -11,7 +11,7 @@ if [[ $stat = windowunmap ]]; then
 	if [[ $cur -ge 1 ]]; then
 		bspc config -m $monitor top_padding $(( $height + $cur ))
 	else
-		bspc config -m $monitor top_padding 30
+		bspc config -m $monitor top_padding 24
 	fi
 else
 	thing="windowunmap" &&
