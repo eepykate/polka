7f8c257d49b6ffb2a995479e01c8f59924079661
Gauge Krahe <gaugekrahe@gmail.com>
2019-07-03 17:36:59 +1000

    Added new tab override javascript

--- .mozilla/firefox/gauge.gauge/chrome/userChrome.js
+++ .mozilla/firefox/gauge.gauge/chrome/userChrome.js
@@ -1,13 +1,3 @@
-// Copyright (c) 2017 Haggai Nuchi
-// Available for use under the MIT License:
-// https://opensource.org/licenses/MIT
-
-// Set "useLionFullScreen" in the same way that it's done in
-// chrome://browser/content/browser-fullScreenAndPointerLock.js
-XPCOMUtils.defineLazyGetter(FullScreen, "useLionFullScreen", function() {
-	return false;
-});
-
 var myFunction = function(e) {
 	const lmb = e.button === 0;
 	if (lmb) {
@@ -29,3 +19,40 @@ function tabClick() {
 
 setInterval(tabClick, 500);
 
+
+
+// ==UserScript==
+// @name                 Custom New Tab
+// @version              1.0
+// @description          Load a custom link or local file, instead of the default new tab page (about:newtab).
+// @author               https://www.reddit.com/user/Luke-Baker/
+// @license              https://creativecommons.org/licenses/by-sa/4.0/
+// @compatibility        Created 2018-01-15. Tested on Firefox 59.
+// ==/UserScript==
+(function() {
+
+	// IMPORTANT: when there's no filename, be sure to include a trailing slash at the end.
+	const mypage = "/home/gauge/.startpage/index.html";
+	// Don't place the caret in the location bar. Useful if you want a page's search box to have focus instead.
+	var removefocus = "yes";
+	// Clear the page's URL from the location bar. Normally not needed, as this should already be the default behavior.
+	var clearlocationbar = "no";
+
+	aboutNewTabService.newTabURL = mypage;
+	function customNewTab () {
+		if (removefocus == "yes") {
+			setTimeout(function() {
+				gBrowser.selectedBrowser.focus();
+			}, 0);
+		}
+		if (clearlocationbar == "yes") {
+			setTimeout(function() {
+				if (gBrowser.selectedBrowser.currentURI.spec == mypage) {
+					window.document.getElementById("urlbar").value = "";
+				}
+			}, 1000);
+		}
+	}
+	gBrowser.tabContainer.addEventListener("TabOpen", customNewTab, false);
+
+}());
