038fed81ee93c7e8437bd7fbaa4d7bfd426e568f
Gauge Krahe <gaugekrahe@gmail.com>
2019-07-04 16:41:56 +1000

    Back on double click to focus urlbar to make it more consistent

--- .mozilla/firefox/gauge.gauge/chrome/tabclick.uc.js
+++ .mozilla/firefox/gauge.gauge/chrome/tabclick.uc.js
@@ -1,20 +1,15 @@
 // ==UserScript==
 // @name                 Tab Click
 // @version              1.0
-// @description          Click the active tab to focus the urlbar
+// @description          Double click the active tab to focus the urlbar
 // @author               https://reddit.com/user/otto251
 // @license              https://creativecommons.org/share-your-work/public-domain/cc0/
 // @compatibility        Created 2019-07-04. Tested on Firefox 69.0b1
 // ==/UserScript==
-var thing = 0;
 var myFunction = function(e) {
-	if (thing === 1) {
-		const lmb = e.button === 0;
-		if (lmb) {
-			document.getElementById('urlbar').focus();
-		}
-	} else {
-		thing = 1;
+	const lmb = e.button === 0;
+	if (lmb) {
+		document.getElementById('urlbar').focus();
 	}
 };
 
@@ -22,11 +17,9 @@ function tabClick() {
 	var classname = document.getElementsByClassName("tabbrowser-tab");
 
 	for (var i = 0; i < classname.length; i++) {
-		classname[i].removeEventListener('click', myFunction);
+		classname[i].removeEventListener('dblclick', myFunction);
 	}
-	thing = 0;
-
-	document.querySelector(".tabbrowser-tab[selected=\"true\"]").addEventListener('click', myFunction);
+	document.querySelector(".tabbrowser-tab[selected=\"true\"]").addEventListener('dblclick', myFunction);
 }
 
 gBrowser.tabContainer.addEventListener("TabSelect", tabClick);
