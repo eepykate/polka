8be8619da69f4abde667a9c184d90c588d81f9a3
Gauge Krahe <gaugekrahe@gmail.com>
2019-07-11 23:01:59 +1000

    Fixed excess padding in c_cal.sh

--- bin/c_cal.sh
+++ bin/c_cal.sh
@@ -2,10 +2,17 @@
 
 day="$(date +%d | sed -e 's/^0/ /')"
 calsh="$(cal | sed -e 's/$/ /g' -e 's/^/ /' )"
-cal="$(echo "$calsh" | sed  -e "s/ $day / \<span color=\'#02a4fc\'\>\<b\>$day\<\/b\>\<\/span\> /" -e '1d' -e '8d' -e 's/ $//g' -e 's/ <\/b><\/span>$/<\/b><\/span>/' -e 's/$/​/' -e 's/^ //')"
-top="$(echo "$calsh" | sed '1!d')"
-cal1="$(echo "$calsh" | sed -e "s/ $day / \\\\e\\[1;34m$day\\\\e\\[0m /" -e '8d' -e 's/ $//' -e 's/ $//' -e 's/ \\e\[0m$/\\e\[0m/' -e 's/$/​/' -e 's/^ //')"
-
-printf "$cal1\n" 
+cal="$(echo "$calsh" | sed  \
+	-e "s/ $day / \<span color=\'#02a4fc\'\>\<b\>$day\<\/b\>\<\/span\> /" \
+	-e '1d' \
+	-e '8d' \
+	-e 's/ <\/b><\/span>$/<\/b><\/span>/' \
+	-e 's/ $//g' \
+	-e 's/^ //g' \
+	-e 's/$/​/')"
+top="$(echo "$calsh" | sed \
+	-e '1!d' \
+	-e 's/ //g' \
+	-e 's/[0-9][0-9]*/ &/')"
 
 notify-send "$top" "$cal"
