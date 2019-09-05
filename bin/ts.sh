83ca1abb77c67740ee245aeb5d1b78f3f226604b
Gauge Krahe <gaugekrahe@gmail.com>
2019-09-05 22:09:54 +1000

    bg{1,2,3,4} and fg{1,2} instead of dark/light/lighter/lightest so it makes more sense

--- bin/ts.sh
+++ bin/ts.sh
@@ -22,79 +22,76 @@ fi
 # Define the colours in the theme (for rofi, startpage, firefox, dunst, etc)
 if [[ $theme = Coral ]]; then
 
-	accentn="5"
-	false="8ac4ba"
-	bgdark="111c23"
-	bglight="17242c"
-	bglighter="1c2d37"
-	bglightest="$(darken $bglighter 1.1)"
-
-	fgdark="999ba0"
-	fglight="edeef0"
-
-	disabled="595959"
-	accent="f6b2b6"
+	#   -----
+	bg1="111c23"
+	bg2="17242c"
+	bg3="1c2d37"
+	bg4="$(darken $bg3 1.1)"
 	button="edeef01a"
-	border="242e35"
-	red="d48398"
+	#   -----
+	fg1="edeef0"
+	fg2="999ba0"
+	disabled="595959"
+	#   -----
+	accent="f6b2b6"
+	false="8ac4ba"
 	hover="000000"
+	#   -----
 
 elif [[ $theme = Withered ]]; then
 
-	accentn="1"
-	false="7bb854"
-	bgdark="f8efe8"
-	bglight="F2E9E3"
-	bglighter="E6DDD6"
-	bglightest=""
-
-	fgdark="999999"
-	fglight="4D4D4D"
-
-	disabled="595959"
-	accent="c48b66"
+	#   -----
+	bg1="f8efe8"
+	bg2="F2E9E3"
+	bg3="E6DDD6"
+	bg4="$(darken $bg3 0.97)"
 	button="4D4D4D16"
-	border="b0b4bb"
-	red="e09b70"
+	#   -----
+	fg1="4D4D4D"
+	fg2="999999"
+	disabled="595959"
+	#   -----
+	accent="c48b66"
+	false="7bb854"
 	hover="f8efe8"
+	#   -----
 
 elif [[ $theme = Snow ]]; then
 
-	accentn="1"
-	false="7bb854"
-	bgdark="f7f9ff"
-	bglight="eef0f5"
-	bglighter="e1e4ea"
-	bglightest="dadde8"
-
-	fgdark="909cad"
-	fglight="43566f"
-
-	disabled="595959"
-	accent="cd5e79"
+	#   -----
+	bg1="f7f9ff"
+	bg2="eef0f5"
+	bg3="e1e4ea"
+	bg4="dadde8"
 	button="43566f1a"
-	border="b0b4bb"
-	red="e09b70"
+	#   -----
+	fg1="43566f"
+	fg2="909cad"
+	disabled="595959"
+	#   -----
+	accent="cd5e79"
+	false="7bb854"
 	hover="ffffff"
+	#   -----
+
 
 elif [[ $theme = Frost ]]; then
 
-	accentn="5"
-	false="e9799b"
-	bgdark="232836"
-	bglight="282e3f"
-	bglighter="2f364a"
-	bglightest="$(darken $bglighter 1.1)"
-
-	fgdark="8686a4"
-	fglight="ccccfa"
-
-	disabled="696969"
-	accent="8da4eb"
+	#   -----
+	bg1="232836"
+	bg2="282e3f"
+	bg3="2f364a"
+	bg4="$(darken $bg3 1.1)"
 	button="ccccfa1a"
-	border="$bglighter"
-	red="dc8189"
+	#   -----
+	fg1="ccccfa"
+	fg2="8686a4"
+	disabled="696969"
+	#   -----
+	accent="8da4eb"
+	false="e9799b"
 	hover="ffffff"
+	#   -----
 
 else
 	echo "Invalid theme; exiting"; exit
@@ -131,11 +128,11 @@ if [[ -n "$theme" ]]; then
 ${XDG_CONFIG_HOME:-~/.config}/.mozilla/firefox/gauge.gauge/chrome/userContent.css
 ${XDG_DATA_HOME:-~/.local/share}/startpage/style.css" | \
 		xargs sed --follow-symlinks -i \
-		-e "s/.*--bgdark:.*#.*\;/--bgdark: #$bgdark\;/" \
-		-e "s/.*--bglight:.*#.*\;/--bglight: #$bglight\;/" \
-		-e "s/.*--bglighter:.*#.*\;/--bglighter: #$bglighter\;/" \
-		-e "s/.*--fgdark:.*#.*\;/--fgdark: #$fgdark\;/" \
-		-e "s/.*--fglight:.*#.*\;/--fglight: #$fglight\;/" \
+		-e "s/.*--bg1:.*#.*\;/--bg1: #$bg1\;/" \
+		-e "s/.*--bg2:.*#.*\;/--bg2: #$bg2\;/" \
+		-e "s/.*--bg3:.*#.*\;/--bg3: #$bg3\;/" \
+		-e "s/.*--fg2:.*#.*\;/--fg2: #$fg2\;/" \
+		-e "s/.*--fg1:.*#.*\;/--fg1: #$fg1\;/" \
 		-e "s/.*--accent:.*#.*\;/--accent: #$accent\;/" \
 		-e "s/.*--border:.*#.*\;/--border: #$border\;/" \
 		-e "s/.*--button:.*#.*\;/--button: #$button\;/" \
@@ -144,21 +141,21 @@ ${XDG_DATA_HOME:-~/.local/share}/startpage/style.css" | \
 
 	# Replace colours in dunst
 	sed --follow-symlinks -i \
-		-e "s/frame_color = \".*\"/frame_color = \"#$fgdark\"/" \
-		-e "s/background = \".*\"/background = \"#$bgdark\"/" \
-		-e "s/foreground = \".*\"/foreground = \"#$fglight\"/"  \
+		-e "s/frame_color = \".*\"/frame_color = \"#$fg2\"/" \
+		-e "s/background = \".*\"/background = \"#$bg1\"/" \
+		-e "s/foreground = \".*\"/foreground = \"#$fg1\"/"  \
 		${XDG_CONFIG_HOME:-~/.config}/dunst/dunstrc
 
 	# Make urgent notifications have a different colour
 	#dunst_urgent="$(( $(awk '/urgency_critical/ {print NR}' ${XDG_CONFIG_HOME:-~/.config}/dunst/dunstrc) + 1 ))"
-	#sed --follow-symlinks -i "${dunst_urgent}s/background = \".*\"/background = \"#$fglight\"/" ${XDG_CONFIG_HOME:-~/.config}/dunst/dunstrc
-	#sed --follow-symlinks -i "$(( ${dunst_urgent} + 1 ))s/foreground = \".*\"/foreground = \"#$bglight\"/" ${XDG_CONFIG_HOME:-~/.config}/dunst/dunstrc
+	#sed --follow-symlinks -i "${dunst_urgent}s/background = \".*\"/background = \"#$fg1\"/" ${XDG_CONFIG_HOME:-~/.config}/dunst/dunstrc
+	#sed --follow-symlinks -i "$(( ${dunst_urgent} + 1 ))s/foreground = \".*\"/foreground = \"#$bg2\"/" ${XDG_CONFIG_HOME:-~/.config}/dunst/dunstrc
 
 	pkill -9 dunst; dunst &>/dev/null &!
 
 	# Change the theme in rofi
-	sed --follow-symlinks -i -e "s/bg:.*#.*;/bg:         #${bgdark};/g" \
-		-e "s/fg:.*#.*;/fg:         #$fglight;/" \
+	sed --follow-symlinks -i -e "s/bg:.*#.*;/bg:         #${bg1};/g" \
+		-e "s/fg:.*#.*;/fg:         #$fg1;/" \
 		-e "s/accent:.*#.*;/accent:     #$accent;/"\
 		-e "s/sel:.*#.*;/sel:        #$button;/"\
 		${XDG_CONFIG_HOME:-~/.config}/rofi/theme.rasi
@@ -170,11 +167,11 @@ ${XDG_DATA_HOME:-~/.local/share}/startpage/style.css" | \
 
 	# Change lemonbar colours
 	sed --follow-symlinks -i \
-		-e "s/bg=\".*\"/bg=\"$bgdark\"/" \
-		-e "s/bl=\".*\"/bl=\"$bglight\"/" \
-		-e "s/blr=\".*\"/blr=\"$bglighter\"/" \
-		-e "s/fg=\".*\"/fg=\"$fglight\"/" \
-		-e "s/fd=\".*\"/fd=\"$fgdark\"/" \
+		-e "s/bg=\".*\"/bg=\"$bg1\"/" \
+		-e "s/bl=\".*\"/bl=\"$bg2\"/" \
+		-e "s/blr=\".*\"/blr=\"$bg3\"/" \
+		-e "s/fg=\".*\"/fg=\"$fg1\"/" \
+		-e "s/fd=\".*\"/fd=\"$fg2\"/" \
 		-e "s/ac=\".*\"/ac=\"$accent\"/" \
 		~/bin/bar
 	# bar &!         # This is now in my bspwmrc
@@ -182,17 +179,17 @@ ${XDG_DATA_HOME:-~/.local/share}/startpage/style.css" | \
 # -e "s/focused_border_color \"#.*\"/focused_border_color \"#$accent\"/g" \
 	# Change bspwm colours
 	sed --follow-symlinks -i \
-		-e "s/normal_border_color \"#.*\"/normal_border_color \"#$fgdark\"/g" \
-		-e "s/focused_border_color \"#.*\"/focused_border_color \"#$fglight\"/g" \
+		-e "s/normal_border_color \"#.*\"/normal_border_color \"#$fg2\"/g" \
+		-e "s/focused_border_color \"#.*\"/focused_border_color \"#$fg1\"/g" \
 		${XDG_CONFIG_HOME:-~/.config}/bspwm/bspwmrc
 	wm restart
 
 	# Change qview colours
 	sed --follow-symlinks -i \
-		-e "s/bgcolor=.*/bgcolor=#$bgdark/" \
+		-e "s/bgcolor=.*/bgcolor=#$bg1/" \
 		${XDG_CONFIG_HOME:-~/.config}/qView/qView.conf
 
-	#walgen "#$bglighter"
+	#walgen "#$bg3"
 
 #	[[ -d ~/git/usr/icons/$theme/ ]] &&
 #		[[ -d ~/usr/icons/Papirus-Dark ]] &&
@@ -200,8 +197,8 @@ ${XDG_DATA_HOME:-~/.local/share}/startpage/style.css" | \
 
 	cp $HOME/usr/icons/folder-blue.svg $HOME/usr/icons/Papirus-Dark/32x32/places/
 	sed -i --follow-symlinks \
-		-e "s|000000|$fglight|g" \
-		-e "s|222222|$(darken $fglight 0.9)|g" \
+		-e "s|000000|$fg1|g" \
+		-e "s|222222|$(darken $fg1 0.9)|g" \
 		$HOME/usr/icons/Papirus-Dark/32x32/places/folder-blue.svg
 
 
@@ -217,20 +214,20 @@ ${XDG_DATA_HOME:-~/.local/share}/startpage/style.css" | \
 		22x22/emblems/emblem-unreadable.svg;
 	do
 		sed -i --follow-symlinks \
-			"s/fill:#[[a-zA-Z0-9][a-zA-Z0-9]*/fill:#$bgdark/" \
+			"s/fill:#[[a-zA-Z0-9][a-zA-Z0-9]*/fill:#$bg1/" \
 			${XDG_DATA_HOME:-~/.local/share}/icons/Papirus-Dark/$f
 	done
 
 
 	#cp ~/opt/Wallpapers/rectangles.svg ~/opt/Wallpapers/tile.svg
 	#sed --follow-symlinks -i \
-	#	-e "s/000000/$bglightest/g" \
-	#	-e "s/222222/$bglighter/g" \
+	#	-e "s/000000/$bg4/g" \
+	#	-e "s/222222/$bg3/g" \
 	#	~/opt/Wallpapers/tile.svg
 	#inkscape -e ~/opt/Wallpapers/tile.png ~/opt/Wallpapers/tile.svg
 	#feh --bg-tile --no-fehbg ~/opt/Wallpapers/tile.png
 
-	walgen1 "#$bglighter"
+	walgen1 "#$bg3"
 
 
 	# Reload gtk theme - probably a major hack
@@ -250,7 +247,7 @@ ${XDG_DATA_HOME:-~/.local/share}/startpage/style.css" | \
 	#refresh
 
 
-	#walgen "#$bglighter"
+	#walgen "#$bg3"
 
 	notify-send "Theme changed to $theme"
 
