ed50d8ae658c0afb1f61b1b11133030fcb9702e1
Gauge Krahe <gaugekrahe@gmail.com>
2019-12-13 15:40:54 +1000

    remove /etc/environment stuff as it does nothing lol

--- other/bin/install.sh
+++ other/bin/install.sh
@@ -339,17 +339,4 @@ sed -i 's/load-module module-esound-protocol-unix/#load-module module-esound-pro
 echo "Telling linux to use no more than 10M ram for caching writes"
 echo 10000000 > /proc/sys/vm/dirty_bytes
 
-if ! grep XDG_CONFIG_HOME /etc/environment &>/dev/null; then
-	echo '
-XDG_CONFIG_HOME="$HOME/etc"
-XDG_CACHE_HOME="$HOME/usr/cache"
-XDG_DATA_HOME="$HOME/usr"' >> /etc/environment
-fi
-
-echo 'ZDOTDIR="$HOME/etc/zsh"' >> /etc/environment
-
-
-
-
-
 echo 'finished'
