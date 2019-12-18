1d2cd0d2806149172a4a467bc236deee969f90cb
Gauge Krahe <gaugekrahe@gmail.com>
2019-12-18 14:33:20 +1000

    Revert "made dropdown.sh use {wm,disp}utils to speed up a bit (0.07s vs 0.03s)" because pfd seems to be inaccurate at times
    This reverts commit 5a96d75a37b5d8370cc2037e1ab13e11cd826315.

--- wm/bin/dropdown.sh
+++ wm/bin/dropdown.sh
@@ -1,15 +1,11 @@
-#!/bin/sh
-#
-# Requires
-# - xdotool
-# - disputils
-# - wmutils
-#
+#!/usr/bin/env bash
+#set -x
+# tabbed -r 2 st -w '' -n "drop-down-st"
+#set -x
 
-[ -f /tmp/tabbed-status ] || touch /tmp/tabbed-status
+[[ -f /tmp/tabbed-status ]] || touch /tmp/tabbed-status
 
-# query wether to hide or show the term
-if [ "$(cat /tmp/tabbed-status)" = windowunmap ]; then
+if [[ $(command cat /tmp/tabbed-status) = windowunmap ]]; then
 	thing="windowmap" &&
 	echo "windowmap" > /tmp/tabbed-status
 else
@@ -17,24 +13,39 @@ else
 	echo "windowunmap" > /tmp/tabbed-status
 fi
 
-# Get screen height and width
-sw="$(dattr w "$(pfd)")"
-sh="$(dattr h "$(pfd)")"
-sx="$(dattr x "$(pfd)")"
-sy="$(dattr y "$(pfd)")"
-
-# un/hide the window
 xdotool search --classname "drop-down-st" $thing
 
-# get width and height of the focused window
-ww="$(wattr w "$(pfw)")"
-wh="$(wattr h "$(pfw)")"
+## Move to the center of the screen -- Credit to https://askubuntu.com/a/571711
+xrandr="$(xrandr -q)"
+# Get screen height and width
+IFS='x' read screenWidth screenHeight < <(xdpyinfo | grep dimensions | grep -o '[0-9x]*' | head -n1)
+width=$(xdotool getactivewindow getwindowgeometry --shell | head -4 | tail -1 | sed 's/[^0-9]*//')
+height=$(xdotool getactivewindow getwindowgeometry --shell | head -5 | tail -1 | sed 's/[^0-9]*//')
+
+#cur=( $(xdotool getmouselocation | grep -o '[0-9][0-9]*' ) )
+
+cur=( $(xdotool getwindowfocus getwindowgeometry | grep -io '[0-9][0-9]*,[0-9][0-9]*' | sed 's/,/ /g') )
+
+#xrandr=( $(xrandr --query | grep ' connected' | grep -o '[0-9][0-9]*x[0-9][0-9]*+[0-9][0-9]*' | sed 's/[x+]/ /g') )
+#xrandr=""
+for x in $(grep -i ' connected' <<< $xrandr | awk '{print $1}'); do
+	xr=( $(grep -e "$x"  <<< $xrandr | grep ' connected' | grep -o '[0-9][0-9]*x[0-9][0-9]*+[0-9][0-9]*+[0-9][0-9]*' | sed 's/[x+]/ /g') )
+	[[ ${cur[1]} -le $(( ${xr[1]} + ${xr[3]} )) ]] &&
+	[[ ${cur[0]} -le $(( ${xr[0]} + ${xr[2]} )) ]] &&
+		break
+done
 
 # Get the middle of the primary screen
-newPosX=
-newPosY=
+# xrandr -q --current | grep -i 'primary' &>/dev/null || xd="single"
+#primary="$(grep -i "$x" <<< $xrandr | grep -o '[0-9][0-9]*x[0-9][0-9]*+[0-9][0-9]*+[0-9][0-9]*' | sed 's/[x+]/ /g')"
+#offsetX="$(($(echo $primary | awk '{print $3}') / 2 ))"
+#offsetY="$(($(echo $primary | awk '{print $4}') / 2 ))"
 
-# move the window to the center of the screen
-xdotool search --classname "drop-down-st" \
-	windowmove "$((sw + sx - $((sw / 2)) - $((ww / 2))))" \
-	"$((sh + sy - $((sh / 2)) - $((wh / 2))))"
+#newPosX=$((screenWidth/2-width/2+$offsetX))
+#newPosY=$((screenHeight/2-height/2+$offsetY))
+
+newPosX="$(( ${xr[0]} + ${xr[2]} - $((${xr[0]} / 2)) - $(($width / 2)) ))"
+newPosY="$(( ${xr[1]} + ${xr[3]} - $((${xr[1]} / 2)) - $(($height / 2 )) ))"
+
+
+xdotool search --classname "drop-down-st" windowmove "$newPosX" "$newPosY"
