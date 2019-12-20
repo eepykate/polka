97fb1992f7c8ecd38570b5f54b74c9ae221563da
Gauge Krahe <gaugekrahe@gmail.com>
2019-12-20 23:23:31 +1000

    Remove a few scripts (ket, ket2, nottime, preview, select_link.sh, select_st, unicornfart, volbar, bar{0,1,2,4,5} (rename bar3 to bar) borders, swirls.sh

--- other/bin/select_link.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-xclip -o -selection primary | xargs xdg-open
--- x/bin/swirls.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cp ~/opt/Wallpapers/swirls.svg ~/opt/Wallpapers/pap.svg
-sed --follow-symlinks -i \
-	-e "s/000000/$bg4/g" \
-	-e "s/222222/$bg2/g" \
-	~/opt/Wallpapers/pap.svg
-inkscape -e ~/opt/Wallpapers/pap.png ~/opt/Wallpapers/pap.svg
-feh --bg-tile --no-fehbg ~/opt/Wallpapers/pap.png
