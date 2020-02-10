bb333a82d3035884d735136d8cc09f90b8d62d92
Gauge Krahe <gaugekrahe@gmail.com>
2020-02-10 18:00:24 +1000

    Make ttycol-gen more verbose and fix it not sending the escape sequences to the correct tty

--- other/bin/ttycol-gen.sh
+++ other/bin/ttycol-gen.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env zsh
 
-template="echo -en \"\\e]P0xd\"  # black
-echo -en \"\\e]P1xd\"  # darkred
-echo -en \"\\e]P2xd\"  # darkgreen
-echo -en \"\\e]P3xd\"  # brown
-echo -en \"\\e]P4xd\"  # darkblue
-echo -en \"\\e]P5xd\"  # darkmagenta
-echo -en \"\\e]P6xd\"  # darkcyan
-echo -en \"\\e]P7xd\"  # lightgrey
-echo -en \"\\e]P8xd\"  # darkgrey
-echo -en \"\\e]P9xd\"  # red
-echo -en \"\\e]PAxd\"  # green
-echo -en \"\\e]PBxd\"  # yellow
-echo -en \"\\e]PCxd\"  # blue
-echo -en \"\\e]PDxd\"  # magenta
-echo -en \"\\e]PExd\"  # cyan
-echo -en \"\\e]PFxd\"  # white"
+template="	echo -en \"\\e]P0xd\" > \"\$tty\" # black
+	echo -en \"\\e]P1xd\" > \"\$tty\" # darkred
+	echo -en \"\\e]P2xd\" > \"\$tty\" # darkgreen
+	echo -en \"\\e]P3xd\" > \"\$tty\" # brown
+	echo -en \"\\e]P4xd\" > \"\$tty\" # darkblue
+	echo -en \"\\e]P5xd\" > \"\$tty\" # darkmagenta
+	echo -en \"\\e]P6xd\" > \"\$tty\" # darkcyan
+	echo -en \"\\e]P7xd\" > \"\$tty\" # lightgrey
+	echo -en \"\\e]P8xd\" > \"\$tty\" # darkgrey
+	echo -en \"\\e]P9xd\" > \"\$tty\" # red
+	echo -en \"\\e]PAxd\" > \"\$tty\" # green
+	echo -en \"\\e]PBxd\" > \"\$tty\" # yellow
+	echo -en \"\\e]PCxd\" > \"\$tty\" # blue
+	echo -en \"\\e]PDxd\" > \"\$tty\" # magenta
+	echo -en \"\\e]PExd\" > \"\$tty\" # cyan
+	echo -en \"\\e]PFxd\" > \"\$tty\" # white"
 
+echo "Getting colours from xrdb"
 colours=($(xrdb -query | \
 grep "\*.*color" | \
 sed -e 's/\*//' -e 's/color//' -e 's/\://' -e 's/#//' | \
 sort -n | \
 awk '{print $2}'))
 
+echo "Writing to '$HOME/bin/ttycol.sh'"
 {
 	echo '#!/usr/bin/env bash'
 	echo 'export TERM=linux'
@@ -35,6 +37,9 @@ awk '{print $2}'))
 	echo 'done'
 
 } > "$HOME/bin/ttycol.sh"
+
+echo "Marking ttycol.sh as executable"
+chmod a+x "$HOME/bin/ttycol.sh"
 
 echo "Moving ttycol.sh to /usr/local/bin/"
 sudo mv "$HOME/bin/ttycol.sh" /usr/local/bin/
