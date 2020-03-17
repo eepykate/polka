8e52582f95ba143c855cea37f4c48e853251ec50
Gauge Krahe <gaugekrahe@gmail.com>
2020-03-18 07:47:51 +1000

    small changes in ts.sh

--- other/bin/ts.sh
+++ other/bin/ts.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 
-opts="$(getopt -o t:w --long theme:,wallpaper -- "$@")"
-eval set -- "$opts"
-while true; do
-	case $1 in
-		-t|--theme) theme="$2"; shift 2;;   # Select theme without using rofi
-		-w|--wallpaper) wallpaper="sure"; shift;;   # use rofi to select a wallpaper
-		--) shift; break;;
+while [ "$1" ]; do
+	case "$1" in
+		-t|--theme) theme="$2"; shift 2;;
+		*) break;;
 	esac
 done
 
-themes="$(ls -1 ~/etc/colours/)" # List of themes
+themes="$(ls -1 ~/etc/colours/ | grep -iv 'current\|css')" # List of themes
 if [[ -z $theme ]]; then
 	theme="$(echo -e "$themes" | rofi -dmenu -i -p "What theme would you like to use?")" \
 		|| exit
 fi
-[[ -n $wallpaper ]] && wallpaper="$(ls $HOME/Wallpapers/*.png | sed 's/.*\///' | rofi -dmenu -i -p "Which Wallpaper?")" # Find png wallpapers in ~/Wallpapers
 ext() {
 	echo "Invalid theme ($theme); exiting"; exit
 }
@@ -23,11 +19,31 @@ ext() {
 # Define the colours in the theme (for rofi, startpage, firefox, dunst, etc)
 [[ -f "${XDG_CONFIG_HOME:-~/.config}/colours/$theme" ]] &&
 	. "${XDG_CONFIG_HOME:-~/.config}/colours/$theme" ||
-	ext
+	{ echo "Invalid theme '$theme'; exiting"; exit; }
 
 echo -e "Theme chosen: $theme\n"
 
 echo "Changing colours in:"
+
+echo " - firefox"
+# Change the colour variables in firefox and my startpage
+sed --follow-symlinks -i \
+	-e "s/--bg0:.*#.*\;/--bg0:      #$bg0\;/" \
+	-e "s/--bg1:.*#.*\;/--bg1:      #$bg1\;/" \
+	-e "s/--bg2:.*#.*\;/--bg2:      #$bg2\;/" \
+	-e "s/--bg3:.*#.*\;/--bg3:      #$bg3\;/" \
+	-e "s/--bg4:.*#.*\;/--bg4:      #$bg4\;/" \
+	-e "s/--fg2:.*#.*\;/--fg2:      #$fg2\;/" \
+	-e "s/--fg1:.*#.*\;/--fg1:      #$fg1\;/" \
+	-e "s/--accent:.*#.*\;/--accent:   #$accent\;/" \
+	-e "s/--false:.*#.*\;/--false:    #$false\;/" \
+	-e "s/--border:.*#.*\;/--border:   #$border\;/" \
+	-e "s/--button:.*#.*\;/--button:   #$button\;/" \
+	-e "s/--hover:.*#.*\;/--hover:    #$hover\;/" \
+	-e "s/--red:.*#.*\;/--red:      #$red\;/" \
+	-e "s/--disabled:.*#.*\;/--disabled: #$disabled\;/" \
+	${XDG_CONFIG_HOME:-~/.config}/.mozilla/firefox/main/chrome/userChrome.css \
+	${XDG_CONFIG_HOME:-~/.config}/.mozilla/firefox/main/chrome/userContent.css
 
 echo " - xresources"
 
@@ -72,35 +88,7 @@ sed --follow-symlinks -i \
 	${XDG_CONFIG_HOME:-~/.config}/Xresources
 
 echo "   * Reloading tabbed and st"
-# Reload terminal colours using Xresources
 rc
-
-echo " - borders"
-sed --follow-symlinks -i               \
-	-e "s/outer=.*/outer='0x$bg1'   # outer/"      \
-	-e "s/inner1=.*/inner1='0x$accent'  # focused/"      \
-	-e "s/inner2=.*/inner2='0x$false'  # normal/"      \
-	~/bin/wm/borders
-
-echo " - firefox"
-# Change the colour variables in firefox and my startpage
-sed --follow-symlinks -i \
-	-e "s/--bg0:.*#.*\;/--bg0:      #$bg0\;/" \
-	-e "s/--bg1:.*#.*\;/--bg1:      #$bg1\;/" \
-	-e "s/--bg2:.*#.*\;/--bg2:      #$bg2\;/" \
-	-e "s/--bg3:.*#.*\;/--bg3:      #$bg3\;/" \
-	-e "s/--bg4:.*#.*\;/--bg4:      #$bg4\;/" \
-	-e "s/--fg2:.*#.*\;/--fg2:      #$fg2\;/" \
-	-e "s/--fg1:.*#.*\;/--fg1:      #$fg1\;/" \
-	-e "s/--accent:.*#.*\;/--accent:   #$accent\;/" \
-	-e "s/--false:.*#.*\;/--false:    #$false\;/" \
-	-e "s/--border:.*#.*\;/--border:   #$border\;/" \
-	-e "s/--button:.*#.*\;/--button:   #$button\;/" \
-	-e "s/--hover:.*#.*\;/--hover:    #$hover\;/" \
-	-e "s/--red:.*#.*\;/--red:      #$red\;/" \
-	-e "s/--disabled:.*#.*\;/--disabled: #$disabled\;/" \
-	${XDG_CONFIG_HOME:-~/.config}/.mozilla/firefox/main/chrome/userChrome.css \
-	${XDG_CONFIG_HOME:-~/.config}/.mozilla/firefox/main/chrome/userContent.css
 
 echo " - dunst"
 # Replace colours in dunst
@@ -109,7 +97,6 @@ dunst_urgent="$(( $(awk '/urgency_critical/ {print NR}' ${XDG_CONFIG_HOME:-~/.co
 
 # low priority notifications
 dunst_low="$(( $(awk '/urgency_low/ {print NR}' ${XDG_CONFIG_HOME:-~/.config}/dunst/dunstrc) + 1 ))"
-
 
 sed --follow-symlinks -i \
 	-e "s/background.*/background          = \"#$bg1\"/" \
@@ -139,58 +126,12 @@ sed --follow-symlinks -i \
 	-e "s/hover:.*#.*;/hover:      #$hover;/"\
 	${XDG_CONFIG_HOME:-~/.config}/rofi/theme.rasi
 
-echo " - bspwm"
-# -e "s/focused_border_color \"#.*\"/focused_border_color \"#$accent\"/g" \
-# Change bspwm colours
-sed --follow-symlinks -i \
-	-e "s/normal_border_color \"#.*\"/normal_border_color \"#$bg1\"/g" \
-	-e "s/focused_border_color \"#.*\"/focused_border_color \"#$bg1\"/g" \
-	${XDG_CONFIG_HOME:-~/.config}/bspwm/bspwmrc
-wm restart
-
 echo " - gtk context menus"
 sed --follow-symlinks -i \
 	-e "s|^	background-color: #.*|	background-color: #$bg3;|" \
 	-e "s|^	color: #.*|	color: #$fg1;|" \
 	${XDG_CONFIG_HOME:-~/.config}/gtk-3.0/menus.css
 
-
-#sed --follow-symlinks -i \
-#	-e "s/color [a-zA-Z0-9][a-zA-Z0-9]*/color $bg1/g" \
-#	-e "s/color [a-zA-Z0-9][a-zA-Z0-9]*/color $fg2/" \
-#	~/etc/conky
-
-#walgen "#$bg3"
-
-#	[[ -d ~/git/usr/icons/$theme/ ]] &&
-#		[[ -d ~/usr/icons/Papirus-Dark ]] &&
-#		cp -f ~/git/usr/icons/$theme/* ~/usr/icons/Papirus-Dark/32x32/places/
-
-#cp $HOME/usr/icons/folder-blue.svg $HOME/usr/icons/Papirus-Dark/32x32/places/
-#sed -i --follow-symlinks \
-#	-e "s|000000|$fg2|g" \
-#	-e "s|222222|$(darken $fg2 0.9)|g" \
-#	$HOME/usr/icons/Papirus-Dark/32x32/places/folder-blue.svg
-
-
-#cp -rf ~/usr/icons/16x16/ ~/usr/icons/Papirus-Dark/
-#cp -r ~/usr/icons/22x22/emblems/* ~/usr/icons/Papirus-Dark/22x22/emblems/
-#for f in \
-#	16x16/devices/drive-harddisk.svg \
-#	16x16/devices/drive-harddisk.svg \
-#	16x16/places/folder.svg \
-#	16x16/devices/drive-removable-media-usb.svg \
-#	16x16/actions/media-eject.svg \
-#	22x22/emblems/emblem-symbolic-link.svg \
-#	22x22/emblems/emblem-unreadable.svg;
-#do
-#	sed -i --follow-symlinks \
-#		-e "s/fill:#[[a-zA-Z0-9][a-zA-Z0-9]*/fill:#$accent/" \
-#		-e "s/stroke:#[[a-zA-Z0-9][a-zA-Z0-9]*/stroke:#$accent/" \
-#		${XDG_DATA_HOME:-~/.local/share}/icons/Papirus-Dark/$f
-#done
-
-#. swirls.sh
 
 echo -e "\nChanging wallpaper"
 	if [[ -f "$HOME/opt/git/Wallpapers/$wall" ]]; then
