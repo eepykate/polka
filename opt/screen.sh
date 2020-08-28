a95db56ca04324f773738fb704cde98cd29e17f9
gk <gaugekrahe@gmail.com>
2020-08-28 18:18:52 +1000

    add screen layout thing for backup

---
+++ opt/screen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+xrandr --output DVI-D-0 --mode 1440x900 --pos 0x0 --rotate normal --output HDMI-0 --primary --mode 1920x1080 --pos 1440x0 --rotate normal --output DP-0 --off --output DP-1 --off
+
+xcalib -c;
+xcalib -red 1 4.5 92 -green 1 4.5 92 -blue 1 4.5 92 -alter
+redshift -o -l 0:0 -m randr:crtc=1 -P -t 7000:7000 -g 0.9:1:1.0 -b 0.9
