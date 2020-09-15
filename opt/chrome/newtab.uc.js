43f87e462caa3cc605757e9c021fc91c2e620f52
gk <gaugekrahe@gmail.com>
2020-09-15 11:19:58 +1000

    cleanup
    new dirs: ~/src and ~/tmp

--- opt/chrome/newtab.uc.js
+++ opt/chrome/newtab.uc.js
@@ -3,6 +3,6 @@ var {classes:Cc,interfaces:Ci,utils:Cu} = Components;
 /* set new tab page */
 try {
   Cu.import("resource:///modules/AboutNewTab.jsm");
-  var newTabURL = "file:///home/mirror/usr/startpage/index.html";
+  var newTabURL = "file:///home/mirror/etc/startpage/index.html";
   AboutNewTab.newTabURL = newTabURL;
 } catch(e){Cu.reportError(e);} // report errors in the Browser Console
